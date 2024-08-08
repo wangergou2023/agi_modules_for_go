ee9447fdabb79ee6c748dcaec85cdb53a31e5480
wangergou2023 <wangergou_2023@qq.com>
2024-08-08 14:33:14 +0800

    tts根据需要开启

--- test/main.go
+++ test/main.go
@@ -15,6 +15,9 @@ import (
 
 var cfg = config.New()
 
+// 定义一个宏控制TTS的使用，因为目前只是生成了mp3文件并没有播放
+const enableTTS = false
+
 func main() {
 	fmt.Println("xiao wan is starting up... Please wait a moment.")
 
@@ -24,12 +27,17 @@ func main() {
 	openaiClient := openai.NewClientWithConfig(config)
 	openaiClient_face := openai.NewClientWithConfig(config)
 	openaiClient_legs := openai.NewClientWithConfig(config)
-	// openaiClient_tts := openai.NewClientWithConfig(config)
+
+	var xiao_wan_chat_tts xiao_wan.Xiao_wan
+
+	if enableTTS {
+		openaiClient_tts := openai.NewClientWithConfig(config)
+		xiao_wan_chat_tts = xiao_wan.StartOne(cfg, openaiClient_tts, xiao_wan.TtsPrompt, "for_after_chat")
+	}
 
 	xiao_wan_chat := xiao_wan.Start(cfg, openaiClient)
 	xiao_wan_chat_face := xiao_wan.StartOne(cfg, openaiClient_face, xiao_wan.FacePrompt, "for_after_chat2")
 	xiao_wan_chat_legs := xiao_wan.StartOne(cfg, openaiClient_legs, xiao_wan.LegsPrompt, "for_after_chat3")
-	// xiao_wan_chat_tts := xiao_wan.StartOne(cfg, openaiClient_tts, xiao_wan.TtsPrompt, "for_after_chat")
 
 	// 启动MQTT订阅
 	go startMQTTClient(&xiao_wan_chat)
@@ -43,12 +51,15 @@ func main() {
 	for {
 		fmt.Print("-> ")
 		text, _ := reader.ReadString('\n')
-		// convert CRLF to LF
 		text = strings.Replace(text, "\n", "", -1)
 		response, _ := xiao_wan_chat.Message(text)
 		fmt.Printf("xiao wan:%s\r\n", response)
-		// response2, _ := xiao_wan_chat_tts.MessageOne(response)
-		// fmt.Printf("xiao wan tts:%s\r\n", response2)
+
+		if enableTTS {
+			response2, _ := xiao_wan_chat_tts.MessageOne(response)
+			fmt.Printf("xiao wan tts:%s\r\n", response2)
+		}
+
 		go xiao_wan_chat_face.MessageOne(response)
 		go xiao_wan_chat_legs.MessageOne(response)
 	}
